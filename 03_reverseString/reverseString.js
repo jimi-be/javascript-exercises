4c125f92499580a9b842715708a6c4005c5f572a
Jim Briggs <jbriggs462@gmail.com>
2022-03-31 20:47:03 -0400

    first (failed) attempt

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reversed ='';
 
+    for (let i = 1; i < input.length; i++) {
+        reversed[i-1] = input.charAt(input.length-1);
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
