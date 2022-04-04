48c418d5e871b364bbe743900699adf93b109a35
Jim Briggs <jbriggs462@gmail.com>
2022-04-03 21:25:32 -0400

    completed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    //console.log(typeof args);
+    // console.log(args);
+    //arr.forEach(element => console.log(element));
+    for (let i = 0;i < arr.length;i++) {
+        if (arr[i] === args[0]) {
+            arr.splice(i,1);
+        }
+    }
+    return arr;
 
+    //delete an element
+    //array.splice(element, number of elements)
 };
+
+// removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
