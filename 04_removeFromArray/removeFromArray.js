b3f81fe0dc89ebfc3441ef746f8beef1002cf9f9
Jim Briggs <jbriggs462@gmail.com>
2022-04-03 21:31:48 -0400

    added foreach loop to complete exercise, clean up

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,14 @@
 const removeFromArray = function(arr, ...args) {
-    //console.log(typeof args);
-    // console.log(args);
-    //arr.forEach(element => console.log(element));
-    for (let i = 0;i < arr.length;i++) {
-        if (arr[i] === args[0]) {
-            arr.splice(i,1);
+    args.forEach(element => {
+        for (let i = 0;i < arr.length;i++) {
+            if (arr[i] === element) {
+                arr.splice(i,1);
+            }
         }
-    }
+    });
+    
     return arr;
-
-    //delete an element
-    //array.splice(element, number of elements)
 };
-
-// removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
