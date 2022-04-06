30510463320cd9cbff7fc7db25ed9d18d251f136
Jim Briggs <jbriggs462@gmail.com>
2022-04-05 21:16:19 -0400

    sum small/large nums, throw error with non-number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
