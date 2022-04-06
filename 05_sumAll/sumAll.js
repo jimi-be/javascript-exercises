96b4644738dc3cd87c6cd14b1849f69452d3d65d
Jim Briggs <jbriggs462@gmail.com>
2022-04-05 21:45:15 -0400

    completed with brute force checks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,18 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    let start = num1;
+    let end = num2;
+    //Error checks
     if (typeof num1 != "number" || typeof num2 != "number") {
         return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    // make sure start is less than end
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
     }
 
     for (let i = start; i <= end; i++) {
@@ -10,7 +21,6 @@ const sumAll = function(num1, num2) {
 
     return sum;
 };
-
 
 // Do not edit below this line
 module.exports = sumAll;
