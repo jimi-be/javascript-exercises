63069c016ad9d53ec417c382d8f38c1b11564dde
Jim Briggs <jbriggs462@gmail.com>
2022-04-09 22:24:50 -0400

    complete ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  // Math.round rounds to nearest integer
+  // Multiply by 10 to grab 1st decimal, round, then
+  // divide by 10 to put decimal in place
+  return Math.round(((fTemp - 32) / 1.8) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  return Math.round(((cTemp * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
