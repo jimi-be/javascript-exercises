cdf6480198a1d2c71c9b5ff88f6dc3ef42ddd7f7
Jim Briggs <jbriggs462@gmail.com>
2022-04-09 22:30:57 -0400

    added notes about the Odin solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,15 @@
 const leapYears = function(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 != 0) {
-            return false;
-        }
-        return true;
-    }
-    return false;
+    //my original solution
+    // if (year % 4 == 0) {
+    //     if (year % 100 == 0 && year % 400 != 0) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // return false;
+    // ----------
+    // simpler to return the result of the if tests directly
+    return year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0);
 };
 
 // Do not edit below this line
